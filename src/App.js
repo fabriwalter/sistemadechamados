02f5e75be73bfa6a1cb5042f753ae9ddccd71524
Walter Fabri <walterfabri18@gmail.com>
2023-07-25 19:52:19 -0300

    criado funcionalidade que cadastra usuarios e adicionado toastify para alertas

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import RoutesApp from './routes';
 
 import AuthProvider from './contexts/auth';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
+        <ToastContainer autoClos={3000} />
         <RoutesApp />
+
       </AuthProvider>
     </BrowserRouter>
   );
--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,22 +1,75 @@
 import { useState, createContext, useEffect } from 'react';
+import { auth, db } from '../services/firebaseConnection'; 
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
+
+import { useNavigate } from 'react-router-dom';
+
+import { toast } from 'react-toastify';
 
 export const AuthContext = createContext({});
 
 function AuthProvider({children}) {
     const [user, setUser] = useState(null);
+    const [loadingAuth, setLoadingAuth] = useState(false);
+
+    const navigate = useNavigate();
 
     function signIn(email, password) {
         console.log(email);
         console.log(password);
         alert('LOGADO COM SUCESO');
     }
+
+    // Cadastrar
+    async function signUp(email, password, name) {
+        setLoadingAuth(true);
+
+        await createUserWithEmailAndPassword(auth, email, password)
+        .then( async (value) => {
+            let uid = value.user.uid
+
+            await setDoc(doc(db, "users", uid), {
+                nome: name,
+                avatarUrl: null,
+
+            })
+            .then( () => {
+                
+                let data = {
+                    uid: uid,
+                    nome: name,
+                    email: value.user.email,
+                    avatarUrl: null
+                };
+
+                setUser(data);
+                storageUser(data);
+                setLoadingAuth(false);
+                toast.success('Seja bem-vindo ao sismtema!');
+                navigate("/dashboard");
+
+            } )
+        })
+        .catch((e) => {
+            console.log(e);
+            setLoadingAuth(false);
+        });
+        
+    }
     
+    function storageUser(data) {
+        localStorage.setItem("@ticketsPRO", JSON.stringify(data));
+    }
+
     return(
         <AuthContext.Provider 
         value={{
             signed: !!null,
             user,
-            signIn
+            signIn,
+            signUp,
+            loadingAuth
         }}>
             {children}
         </AuthContext.Provider>
