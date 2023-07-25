ee8617bdcf8cfc0253e850cb980828f8b963dc1f
Walter Fabri <walterfabri18@gmail.com>
2023-07-25 13:48:02 -0300

    criado estrutura da pagina de login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,42 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import './signin.css';
+
+import logo from '../../assets/logo.png';
+
 export default function SignIn() {
+    
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState(''); 
+
     return(
-        <div>
-            <h1>Página Login</h1>
+        <div className='container-center'>
+            <div className='login'>
+                <div className='login-area'>
+                    <img src={logo} alt='Logo do sistema de chamados' />
+                </div>
+
+                <form>
+                    <h1>Entrar</h1>
+
+                    <input 
+                    type='text' 
+                    placeholder='Digite o seu e-mail...'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value) } />
+
+                    <input 
+                    type='password'
+                    placeholder='************'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value) } />
+
+                    <input type='submit' value='Acessar' />
+                </form>
+
+                <Link to="/register">Criar uma conta</Link>                
+            </div>
         </div>
     );
 }
