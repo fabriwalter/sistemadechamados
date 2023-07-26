ac84ec1e63107b1a8e401b4a7522b01b8bc3632a
Walter Fabri <walterfabri18@gmail.com>
2023-07-26 11:30:02 -0300

    criado funcionalidade de login

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,6 +1,6 @@
 import { useState, createContext, useEffect } from 'react';
-import { auth, db } from '../services/firebaseConnection'; 
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth, db, storage } from '../services/firebaseConnection'; 
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
 
 import { useNavigate } from 'react-router-dom';
@@ -15,10 +15,34 @@ function AuthProvider({children}) {
 
     const navigate = useNavigate();
 
-    function signIn(email, password) {
-        console.log(email);
-        console.log(password);
-        alert('LOGADO COM SUCESO');
+    async function signIn(email, password) {
+        setLoadingAuth(true);
+
+        await signInWithEmailAndPassword(auth, email, password)
+        .then( async (value) => {
+            let uid = value.user.uid;
+
+            const docRef = doc(db, "users", uid);
+            const docSnap = await getDoc(docRef);
+
+            let data = {
+                uid: uid, 
+                nome: docSnap.data().nome,
+                email: value.user.email,
+                avatarUrl: docSnap.data().avatarUrl
+            }
+
+            setUser(data);
+            storageUser(data);
+            setLoadingAuth(false);
+            toast.success('Bem-vindo(a) de volta');
+            navigate("/dashboard");
+        })
+        .catch((e) => {
+            console.log('Existe um erro: ' + e);
+            setLoadingAuth(false);
+            toast.error('Ops, algo de errado! Tente novamente');
+        });
     }
 
     // Cadastrar
