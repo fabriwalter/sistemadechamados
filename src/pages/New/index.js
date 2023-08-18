0b1a4d7e17361072e994fcefc88cd2c63ae637bc
Walter Fabri <walterfabri18@gmail.com>
2023-08-18 12:05:24 -0300

    finalizada logica de buscar clientes no db

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,22 +1,74 @@
-import { useState } from 'react';
+import { useState, useEffect, useContext } from 'react';
 
 import Header from '../../components/Header';
 import Title from '../../components/Title';
 
 import { FiPlusCircle } from 'react-icons/fi'
 
+import { AuthContext } from '../../contexts/auth';
+import { db } from '../../services/firebaseConnection';
+import { collection, getDocs, getDoc, doc } from 'firebase/firestore';
+
 import './new.css';
 
+const listRef = collection(db, "customers");
+
 export default function New() {
-    
+    const { user } = useContext(AuthContext);
+
     const [customers, setCustomers] = useState([]);
-    
+    const [loadCustomer, setLoadCustomer] = useState(true);
+    const [customerSelected, setCustomerSelected] = useState(0);
+
     const [complemento, setComplemento] = useState('');
     const [assunto, setAssunto] = useState('Suporte');
     const [status, setStatus] = useState('Aberto');
 
+    useEffect(() => {
+        async function loadCustomers() {
+            const querySnapshot = await getDocs(listRef)
+            .then( (snapshot) => {
+                let lista = [];
+
+                snapshot.forEach((doc) => {
+                    lista.push({
+                        id: doc.id,
+                        nomeFantasia: doc.data().nomeFantasia
+                    })
+                })
+
+                if (snapshot.docs.size === 0){
+                    console.log('Nenhuma empresa encontrada!');
+                    setCustomers([ { id: '1', nomeFantasia: 'FREELA' } ]);
+                    setLoadCustomer(false);
+                    return;
+                }
+
+                setCustomers(lista);
+                setLoadCustomer(false);
+
+            })
+            .catch( (e) => {
+                console.log("ERRO AO BUSCAR OS CLIENTES", e);
+                setLoadCustomer(false);
+                setCustomers([ { id: '1', nomeFantasia: 'FREELA' } ])
+            })
+        }
+
+        loadCustomers();
+    }, [])
+
     function handleOptionChange(e) {
         setStatus(e.target.value);
+    }
+
+    function handleChangeSelect(e) {
+        setAssunto(e.target.value);
+    }
+
+    function handleChangeCustomer(e) {
+        setCustomerSelected(e.target.value);
+        console.log(customers[e.target.value].nomeFantasia);
     }
 
 
@@ -32,13 +84,25 @@ export default function New() {
                     <form className='form-profile'>
                         
                         <label>Clientes</label>
-                        <select>
-                            <option key={1} value={1}>Mercado Teste</option>
-                            <option key={2} value={2}>Loja Informaticae</option>
-                        </select>
+                        {
+                            loadCustomer ? (
+                                <input type='text' disabled={true} value="Carregando..." />
+                            ) : (
+                                <select value={customerSelected} onChange={handleChangeCustomer}>
+                                    {customers.map((item, index) => {
+                                        return(
+                                            <option key={index} value={index}>
+                                                {item.nomeFantasia}
+                                            </option>
+                                        )
+                                    })}
+                                </select>
+                            )
+                            
+                        }
 
                         <label>Assunto</label>
-                        <select>
+                        <select value={assunto} onChange={handleChangeSelect}>
                             <option key={1} value="Suporte">Suporte</option>
                             <option key={2} value="Visita Tecnica">Visita Técnica</option>
                             <option key={3} value="Financeiro">Financeiro</option>
